3976314e577b872de134782d27f1b30f0b6d4521
MikiWojak <mikizarowa8@gmail.com>
2021-05-10 10:54:34 +0200

    Get Token using custom Endpoint
    - Install GuzzleHttp to handle the requests
    - Create an AuthCOntroller to handle the requests
    - Code getting the endpoint in the backend - hardcode
    - Works properly

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+
 Route::get('/todos', 'App\Http\Controllers\TodosController@index');
 Route::post('/todos', 'App\Http\Controllers\TodosController@store');
 Route::patch('/todos/{todo}', 'App\Http\Controllers\TodosController@update');
